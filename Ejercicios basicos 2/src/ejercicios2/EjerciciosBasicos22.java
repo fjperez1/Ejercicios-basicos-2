7842d8a062d52a0765e2f23eedffa8d946e74f71
fjperez1 <francisco.perez@iesnervion.es>
2022-09-27 12:35:01 +0200

    Ejercicio 2

---
+++ Ejercicios basicos 2/src/ejercicios2/EjerciciosBasicos22.java
@@ -0,0 +1,24 @@
+package ejercicios2;
+
+import java.util.Scanner;
+
+public class EjerciciosBasicos22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declaramos las variables que vamos a utilizar
+		
+		int edad;
+		Scanner lectura = new Scanner(System.in);
+		
+		//Solicitamos al usuario que nos indique su edad
+		
+		System.out.print("Introduce tu edad guap@");
+		edad = lectura.nextInt();
+		
+		System.out.println(edad>=18);
+
+	}
+
+}
