7d957ebf4a473980f1feb4389a3f5c0cd72fd194
fjperez1 <francisco.perez@iesnervion.es>
2022-09-28 12:38:40 +0200

    Ejercicio 4

---
+++ Ejercicios basicos 2/src/ejercicios2/EjerciciosBasicos24.java
@@ -0,0 +1,29 @@
+package ejercicios2;
+
+import java.util.Scanner;
+
+public class EjerciciosBasicos24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean llueve;
+		boolean tareas;
+		boolean biblio;
+		Scanner lectura = new Scanner(System.in);
+		
+		System.out.println("¿Llueve?");
+		llueve = lectura.nextBoolean();
+		
+		System.out.println("¿Has finalizado tus tareas?");
+		tareas = lectura.nextBoolean();
+		
+		System.out.println("¿Necesitas ir a la biblioteca?");
+		biblio = lectura.nextBoolean();
+		
+				
+		System.out.println((llueve==false && tareas==true) || biblio==true);
+
+	}
+
+}
