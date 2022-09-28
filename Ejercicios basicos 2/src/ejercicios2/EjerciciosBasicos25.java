959b8f913c9d269890a3262b54ad8f0e0c59dd86
fjperez1 <francisco.perez@iesnervion.es>
2022-09-28 13:10:10 +0200

    Ejercicio 5

---
+++ Ejercicios basicos 2/src/ejercicios2/EjerciciosBasicos25.java
@@ -0,0 +1,37 @@
+package ejercicios2;
+
+import java.util.Scanner;
+
+public class EjerciciosBasicos25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Declaramos las variables que vamos a utilizar
+		double nprim = 0.0;
+		double nseg = 0.0;
+		double nter = 0.0;
+		double media = 0.0;
+		Scanner lectura = new Scanner(System.in);
+		
+		//Solicitamos al usuario que introduzca las notas
+		
+		System.out.println("Introduce las notas del primer trimestre");
+		nprim = lectura.nextDouble();
+		System.out.println("Introduce las notas del segundo trimestre");
+		nseg = lectura.nextDouble();
+		System.out.println("Introduce las notas del tercer trimestre");
+		nter = lectura.nextDouble();
+		
+		//
+		
+		media = (nprim + nseg + nter)/3;
+		
+		System.out.println("La nota media en boletín es " + (int)media);
+	    System.out.format("La nota media en expediente es %.2f", media);
+
+
+		
+	}
+
+}
